16edbc7e90814f8619790687056c642fa0bb2c21
Patrick Wells <pat.taylorwells@gmail.com>
2020-08-10 11:05:42 -0400

    final

--- Develop/app.js
+++ Develop/app.js
@@ -4,14 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
 let people = [];
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function inquirerPrompt() {
@@ -40,8 +36,7 @@ function inquirerPrompt() {
             internorEngineer(people, input)
         })
 }
-
-function internorEngineer(peopleArray) {
+function internorEngineer() {
     inquirer
         .prompt([{
             type: "input",
@@ -72,6 +67,8 @@ function internorEngineer(peopleArray) {
                             message: "Please enter the intern's university.",
                             name: "university"
                         }]).then((internInfo) => {
+                            console.log('internInfo',internInfo);
+                            console.log('input2',input2);
                             people.push(new Intern(input2.name, input2.id, input2.email, internInfo.university))
                             inquirer
                                 .prompt([{
@@ -79,8 +76,8 @@ function internorEngineer(peopleArray) {
                                     message: "Do you have another Employee to add?",
                                     name: "yn"
                                 }]).then((answer) => {
-                                    if (answer == "yes") {
-                                        internorEngineer(people)
+                                    if (answer.yn == "yes") {
+                                        internorEngineer()
                                     }
                                     else {
                                         fs.writeFile(outputPath, render(people), function (err) {
@@ -93,7 +90,7 @@ function internorEngineer(peopleArray) {
                                 })
                         })
                     // console.log(input2.role[0])
-                    break
+                    break;
                 case "Engineer":
                     inquirer
                         .prompt([{
@@ -101,15 +98,17 @@ function internorEngineer(peopleArray) {
                             message: "Please enter the Github URL",
                             name: "github"
                         }]).then((engInfo) => {
-                            people.push(new Engineer(input.name, input.id, input.email, engInfo.github))
+                            console.log('engInfo',engInfo);
+                            console.log('input2',input2);
+                            people.push(new Engineer(input2.name, input2.id, input2.email, engInfo.github))
                             inquirer
                                 .prompt([{
                                     type: "input",
                                     message: "Do you have another Employee to add?",
                                     name: "yn"
                                 }]).then((answer) => {
-                                    if (answer == "yes") {
-                                        internorEngineer(people)
+                                    if (answer.yn == "yes") {
+                                        internorEngineer()
                                     }
                                     else {
                                         fs.writeFile(outputPath, render(people), function (err) {
@@ -128,24 +127,4 @@ function internorEngineer(peopleArray) {
             }
         })
 }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 inquirerPrompt();
